d8c788b5bc167a27df72727a133ef2a60fefb7bb
mary446 <135317707+mary446@users.noreply.github.com>
2023-06-16 21:11:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph){
+cats.push('Ralph');
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop('Garfield');
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift('Milo');
+}
+function  appendCat(Broom){
+    var cats2 = []
+    cats.push('Broom')
+     return cats
+}
